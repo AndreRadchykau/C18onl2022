4123ac42bf1015623af6f93df83cf691b8982ef7
AndreRadchykau <siabruk1998@gmail.com>
2022-11-24 19:49:25 +0300

    Lesson 7, lesson7 (Computer)

---
+++ Lesson7/src/main/java/by/tms/model/Computer.java
@@ -0,0 +1,86 @@
+package by.tms.model;
+
+import lombok.NoArgsConstructor;
+
+import java.util.Random;
+import java.util.Scanner;
+
+@NoArgsConstructor
+
+public class Computer {
+
+    private String cpu;
+    private String ram;
+    private String ssd;
+    private int resource;
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+    private boolean burned;
+
+    public boolean theBurned() {
+        return burned;
+    }
+
+    public Computer(String cpu, String ram, String ssd, int resource) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.resource = resource;
+    }
+
+    public Computer(int resource) {
+        this.resource = resource;
+    }
+
+    public void on() {
+        if (!burned) {
+            int inputNumber = inputNumber();
+            int count = 0;
+            if (inputNumber == count) {
+                off();
+            } else {
+                burned();
+            }
+        } else {
+            burned();
+        }
+    }
+
+    private void burned() {
+        System.out.println("Компьютер сгорел!");
+        burned = true;
+    }
+
+    private int inputNumber() {
+        System.out.println("Введите 0 или 1");
+        int number = 0;
+        do {
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+            } else {
+                System.out.println("Ошибка!");
+                scanner.next();
+            }
+        } while (number < 0 || number > 1);
+        return number;
+    }
+
+    public void off() {
+        if (!burned && resource != 0) {
+            System.out.println("Выключение");
+            scanner.nextInt();
+        } else {
+            System.out.println("Компьютер сгорел!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "cpu='" + cpu + '\'' +
+                ", ram='" + ram + '\'' +
+                ", ssd='" + ssd + '\'' +
+                ", resource=" + resource +
+                '}';
+    }
+}
---
+++ Lesson7/src/main/java/by/tms/service/Main.java
@@ -0,0 +1,18 @@
+package by.tms.service;
+
+import by.tms.model.Computer;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer("5600X", "16 gb", "SSD 1000 GB", 10);
+        System.out.println(computer);
+        for (int i = 0; i < 10; i++) {
+            if (computer.theBurned()) {
+                break;
+            }
+
+        }
+        computer.on();
+        computer.off();
+    }
+}
